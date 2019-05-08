416028648673548890d8a867a0016ba100ca7a9f
Gabor Majoros <gabormajo@yahoo.com>
2019-05-08 12:23:21 +0200

    threadPoolConfig update libs

--- core-common/src/main/java/eu/arrowhead/common/ArrowheadMain.java
+++ core-common/src/main/java/eu/arrowhead/common/ArrowheadMain.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.ServiceConfigurationError;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import javax.net.ssl.SSLContext;
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.core.UriBuilder;
